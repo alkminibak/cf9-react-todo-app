fd228d519cfe3ed7c8bcd214c9ddf4bac18a0972
alkminibak <alkmini.bak@gmail.com>
2026-05-21 20:36:54 +0300

    feat(todo): add TodoStats component

--- src/features/todo/TodoApp.tsx
+++ src/features/todo/TodoApp.tsx
@@ -2,6 +2,7 @@ import TodoForm from "./TodoForm.tsx";
 import {useRef} from "react";
 import {useTodos} from "./hooks/useTodos.ts";
 import TodoList from "./TodoList.tsx";
+import TodoStats from "./hooks/TodoStats.tsx";
 
 const TodoApp = () => {
    const inputRef = useRef<HTMLInputElement>(null);
@@ -25,6 +26,8 @@ const TodoApp = () => {
                onEdit={editTodo}
                onDelete={deleteTodo}
             />
+
+            <TodoStats todos={todos}/>
 
          </div>
       </>
